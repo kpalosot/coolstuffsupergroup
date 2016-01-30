0f7c728eed6b883392c6ff8fedfc741b7ad52da1
Graphic <lucasbaldi@gmail.com>
2016-01-30 12:31:41 -0500

    Proj

---
+++ Satan3D/Assets/Follow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+	public float speed;
+	private GameObject player;
+	Vector3 direction;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		direction = player.transform.position - this.gameObject.transform.position;
+		transform.position = Vector3.Lerp (transform.position, transform.TransformPoint (direction), 2f * Time.deltaTime);
+
+	}
+}
---
+++ Satan3D/Assets/SphereMove.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMove : MonoBehaviour {
+
+	// Use this for initialization
+	private Rigidbody rb;
+	public float speed;
+	GameObject cube;
+	GameObject pig;
+	GameObject held = null;
+	bool isHolding = false;
+	// Use this for initialization
+	void Start () {
+		//rb = GetComponent<Rigidbody> ();
+		cube = GameObject.Find ("Cube");
+		pig = GameObject.Find ("Pig");
+		//splane = GameObject.Find ("SPlane");
+	}
+	
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey(KeyCode.UpArrow)){ transform.position = Vector3.Lerp(transform.position,transform.TransformPoint(Vector3.forward),10f* Time.deltaTime); }
+		
+		if (Input.GetKey(KeyCode.DownArrow)){ transform.position = Vector3.Lerp(transform.position, transform.TransformPoint (Vector3.back),10f* Time.deltaTime); }
+		
+		if (Input.GetKey(KeyCode.LeftArrow)){ transform.position = Vector3.Lerp(transform.position,transform.TransformPoint(Vector3.left),10f* Time.deltaTime); }
+		
+		if (Input.GetKey(KeyCode.RightArrow)){ transform.position = Vector3.Lerp(transform.position,transform.TransformPoint(Vector3.right),10f* Time.deltaTime); }
+
+
+	}
+	/*(void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}*/
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pickup") && !isHolding){
+			//other.gameObject.SetActive(false);
+			cube.transform.position = transform.position;
+			cube.transform.position = Vector3.Lerp(cube.transform.position, cube.transform.TransformPoint(Vector3.back), 5f);
+			cube.transform.position = Vector3.Lerp(cube.transform.position, cube.transform.TransformPoint(Vector3.up), 15f);
+
+			cube.transform.parent = transform;
+			isHolding = true;
+			held = cube;
+		}
+
+		if (other.gameObject.CompareTag ("Pig") && !isHolding) {
+			pig.transform.position = transform.position;
+			pig.transform.position = Vector3.Lerp(pig.transform.position, pig.transform.TransformPoint(Vector3.forward), 5f);
+			pig.transform.position = Vector3.Lerp(pig.transform.position, pig.transform.TransformPoint(Vector3.up), 15f);
+			
+			pig.transform.parent = transform;
+			isHolding = true;
+			held = pig;
+		}
+
+		if (other.gameObject.CompareTag ("Pentagon") && isHolding) {
+			held.SetActive (false);
+			held = null;
+			isHolding = false;
+		}
+
+		/*if (other.gameObject.name == "Capsule") {
+			this.gameObject.SetActive (false);
+		}*/
+
+		//check collision of Player and Capsule and if Player is holding pig object
+		//might want to change this so that it works for all other objects when we get there
+		if(other.gameObject.CompareTag("Capsule") && isHolding){
+			pig.transform.position = transform.position;
+			pig.transform.position = Vector3.Lerp(pig.transform.position,pig.transform.TransformPoint(Vector3.down),15f);
+			isHolding = false;
+			held = null;
+			//pig.transform.DetachChildren();
+			//pig.transform.parent = splane.transform;
+
+		}
+
+		//collision with other objects(any other object not just pig) followed by a collision of capsule seems to break the game
+		//collision with other objects other than pig makes our player object get stuck with that object
+
+	}
+}
+
