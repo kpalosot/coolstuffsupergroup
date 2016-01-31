0d9b02b630349028f6de717948d409989d55be87
lucasbaldi <lucasbaldi@gmail.com>
2016-01-31 12:49:42 -0500

    full update

--- TerrainMaking/Assets/Scripts/PlayerMove.cs
+++ TerrainMaking/Assets/Scripts/PlayerMove.cs
@@ -5,48 +5,79 @@ public class PlayerMove : MonoBehaviour {
 
 	private Rigidbody rb;
 
+	GameObject pig;
+	GameObject pigBaddy;
+
+	public GameObject[] Baddies;
+
+	GameObject held;
+
 	float speed = 100;
+
+	float distance;
+	float respawnTime;
+
+	bool isHolding;
+	bool canPickup;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+
+		pig = GameObject.Find ("Pig");
+		pigBaddy = GameObject.Find ("PigBaddy");
+
+		Baddies = new GameObject[10];
+		Baddies[0] = pigBaddy; 
+
+
+		held = null;
+
+		isHolding = false;
+		canPickup = true;
 	}
 	
 	// Update is called once per frame
-	/*void Update () {
-		if (Input.GetKey(KeyCode.UpArrow)){
-			//transform.position = Vector3.Lerp(transform.position, transform.TransformPoint(Vector3.Scale(Vector3.forward, allSpeeds[curSpeedInd])), 10f * Time.deltaTime);
-			//transform.position = Vector3.Lerp(transform.position, transform.TransformPoint(Vector3.forward), 10f * Time.deltaTime);
-			rb.AddForce(Vector3.forward * speed);
-		}
-		
-		if (Input.GetKey(KeyCode.DownArrow)){
-			//transform.position = Vector3.Lerp(transform.position, transform.TransformPoint (Vector3.Scale(Vector3.back, allSpeeds[curSpeedInd])), 10f * Time.deltaTime);
-			//transform.position = Vector3.Lerp(transform.position, transform.TransformPoint (Vector3.back), 10f * Time.deltaTime);
-			rb.AddForce(Vector3.back * speed);
-		}
-		
-		if (Input.GetKey(KeyCode.LeftArrow)){
-			//transform.position = Vector3.Lerp(transform.position, transform.TransformPoint(Vector3.Scale(Vector3.left, allSpeeds[curSpeedInd])), 10f * Time.deltaTime);
-			//transform.position = Vector3.Lerp(transform.position, transform.TransformPoint(Vector3.left), 10f * Time.deltaTime);
-			rb.AddForce(Vector3.left * speed);
-		}
-		
-		if (Input.GetKey(KeyCode.RightArrow)){
-			//transform.position = Vector3.Lerp(transform.position, transform.TransformPoint(Vector3.Scale(Vector3.right, allSpeeds[curSpeedInd])), 10f * Time.deltaTime);
-			//transform.position = Vector3.Lerp(transform.position, transform.TransformPoint(Vector3.right), 10f * Time.deltaTime);
-			rb.AddForce(Vector3.right * speed);
-		}
+	void Update () {
+
 	
-	}*/
+	}
 
 	void FixedUpdate ()
 	{
+		foreach (GameObject baddy in Baddies) {
+			canPickup = canPickup && (Vector3.Distance (baddy.transform.position, transform.position) < 3f);
+
+
+		}
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		
 		rb.AddForce (movement * speed);
+	}
+	void LateUpdate(){
+		canPickup = true;
+	}
+
+	void OnTriggerEnter(Collider thing){
+		if (thing.gameObject.CompareTag ("Pig")) {
+			pig.transform.position = transform.position;
+			pig.transform.position = Vector3.Lerp(pig.transform.position, pig.transform.TransformPoint(Vector3.forward), 5f);
+			pig.transform.position = Vector3.Lerp(pig.transform.position, pig.transform.TransformPoint(Vector3.up), 15f);
+
+			pig.transform.parent = transform;
+
+			held = pig;
+
+		}
+		if (thing.gameObject.CompareTag ("Baddy")) {
+			drop();
+		}
+	}
+
+	public void drop(){
+		Debug.Log ("DRRRROP");
 	}
 }
